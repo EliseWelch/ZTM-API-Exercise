f37709ec6fa70ee3ac66c126e1e019bc81f79d49
Elise Welch <eliseanyawelch@gmail.com>
2020-06-22 16:50:08 -0500

    Updates

---
+++ precache-manifest.c7501602dd8e169ae54e0b35f3dc6235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3274fcc0300efb2ac90f00870f159445",
+    "url": "/ZTM-API-Exercise/index.html"
+  },
+  {
+    "revision": "278bb2316efe3f33304b",
+    "url": "/ZTM-API-Exercise/static/css/main.1bb25e5b.chunk.css"
+  },
+  {
+    "revision": "46c8d3349275911df887",
+    "url": "/ZTM-API-Exercise/static/js/2.42dd37ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ZTM-API-Exercise/static/js/2.42dd37ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "278bb2316efe3f33304b",
+    "url": "/ZTM-API-Exercise/static/js/main.21bc3320.chunk.js"
+  },
+  {
+    "revision": "ce64adf0cf0e0b45c8c5",
+    "url": "/ZTM-API-Exercise/static/js/runtime-main.0d5f384f.js"
+  }
+]);
