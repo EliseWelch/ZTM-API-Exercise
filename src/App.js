a8a06dd28be0e1b5e2f4aea7420625fcd5a5ebd5
Elise Welch <eliseanyawelch@gmail.com>
2020-06-22 16:46:14 -0500

    commited

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      answer: ''
+    }
+  }
+
+  zero = ()=> {
+    fetch('http://numbersapi.com/0/trivia?json')
+      .then(response => response.json())
+      .then(data => {this.setState({answer: data.text})});
+  };
+
+  one = ()=> {
+    fetch('http://numbersapi.com/1/trivia?json')
+      .then(response => response.json())
+      .then(data => {this.setState({answer: data.text})});
+  };
+
+  two = ()=> {
+    fetch('http://numbersapi.com/2/trivia?json')
+      .then(response => response.json())
+      .then(data => {this.setState({answer: data.text})});
+  };
+
+  three = ()=> {
+    fetch('http://numbersapi.com/3/trivia?json')
+      .then(response => response.json())
+      .then(data => {this.setState({answer: data.text})});
+  };
+
+  four = ()=> {
+    fetch('http://numbersapi.com/4/trivia?json')
+      .then(response => response.json())
+      .then(data => {this.setState({answer: data.text})});
+  };
+
+  five = ()=> {
+    fetch('http://numbersapi.com/5/trivia?json')
+      .then(response => response.json())
+      .then(data => {this.setState({answer: data.text})});
+  };
+
+
+
+
+  render () {
+    return (
+    <div className='app-content'>
+      <div className='header'>
+        <h1>Number API</h1>
+        <p>Pick A Number Below To See An Intresting Fact About It</p>
+      </div>
+      <div className='buttons'>
+        <button onClick={this.zero}>0</button>
+        <button onClick={this.one}>1</button>
+        <button onClick={this.two}>2</button>
+        <button onClick={this.three}>3</button>
+        <button onClick={this.four}>4</button>
+        <button onClick={this.five}>5</button>
+      </div>
+      <p>{this.state.answer}</p>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
